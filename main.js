50f325b388b80326d3f13849d9c2bef1ce202e21
IbrahimIsmayilov <i.ismayilov@share.epsb.ca>
2022-09-13 14:36:49 -0600

    S

--- main.js
+++ main.js
@@ -16,63 +16,25 @@ function btnClicked() {
   let sciPercent = +document.getElementById('sci-percent').value;
 
   // Calculate & Output Letter Grades
-  // English
-  let engLetter;
-  if (engPercent >= 80) {
-    engLetter = 'A';
-  } else if (engPercent >= 65) {
-    engLetter = 'B';
-  } else if (engPercent >= 55) {
-    engLetter = 'C';
-  } else if (engPercent >= 50) {
-    engLetter = 'D';
-  } else {
-    engLetter = 'F';
-  }
-  document.getElementById('eng-letter').innerHTML = engLetter;
+  document.getElementById('eng-letter').innerHTML = calcGrade(engPercent);
+  document.getElementById('soc-letter').innerHTML = calcGrade(socPercent);
+  document.getElementById('math-letter').innerHTML = calcGrade(mathPercent);
+  document.getElementById('sci-letter').innerHTML = calcGrade(sciPercent);
+}
 
-  // Social Studies
-  let socLetter;
-  if (socPercent >= 80) {
-    socLetter = 'A';
-  } else if (socPercent >= 65) {
-    socLetter = 'B';
-  } else if (socPercent >= 55) {
-    socLetter = 'C';
-  } else if (socPercent >= 50) {
-    socLetter = 'D';
+function calcGrade(percentGrade) {
+  let letterGrade;
+  if (percentGrade >= 80) {
+    letterGrade = 'A'
+  } else if (percentGrade >= 65) {
+    letterGrade = 'B';
+  } else if (percentGrade >= 55) {
+    letterGrade = 'C';
+  } else if (percentGrade >= 50) {
+    letterGrade = 'D';
   } else {
-    socLetter = 'F';
+    letterGrade = 'F';
   }
-  document.getElementById('soc-letter').innerHTML = socLetter;
 
-  // Mathematics
-  let mathLetter;
-  if (mathPercent >= 80) {
-    mathLetter = 'A';
-  } else if (mathPercent >= 65) {
-    mathLetter = 'B';
-  } else if (mathPercent >= 55) {
-    mathLetter = 'C';
-  } else if (mathPercent >= 50) {
-    mathLetter = 'D';
-  } else {
-    mathLetter = 'F';
-  }
-  document.getElementById('math-letter').innerHTML = mathLetter;
-
-  // Science
-  let sciLetter;
-  if (sciPercent >= 80) {
-    sciLetter = 'A';
-  } else if (sciPercent >= 65) {
-    sciLetter = 'B';
-  } else if (sciPercent >= 55) {
-    sciLetter = 'C';
-  } else if (sciPercent >= 50) {
-    sciLetter = 'D';
-  } else {
-    sciLetter = 'F';
-  }
-  document.getElementById('sci-letter').innerHTML = sciLetter;
+  return letterGrade;
 }
